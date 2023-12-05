9cec0503357691c21b00da12cb4b1b95d3fda9a8
4701104091 <4701104091@student.hcmue.edu.vn>
2023-12-05 17:30:27 +0700

    first commit

---
+++ Telescope.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Telescope.Models;
+
+namespace Telescope.DataAccess.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Bút viết", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Tập vở", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Thước kẻ", DisplayOrder = 3 }
+                );
+
+            modelBuilder.Entity<Company>().HasData(
+                new Company
+                {
+                    Id = 1,
+                    Name = "NGUYEN MINH HOANG",
+                    StreetAddress="351A Lạc Long Quân",
+                    Ward="Phường 5",
+                    District ="Quận 11",
+                    Province ="Tp. Hồ Chí Minh",
+                    PostalCode="12121",
+                    PhoneNumber="6669990000"
+                },
+                new Company
+                {
+                    Id = 2,
+                    Name = "TRAN MINH KHOI",
+                    StreetAddress="351A Lạc Long Quân",
+                    Ward="Phường 5",
+                    District ="Quận 11",
+                    Province ="Tp. Hồ Chí Minh",
+                    PostalCode="12121",
+                    PhoneNumber = "1113335555"
+                },
+                new Company
+                {
+                    Id = 3,
+                    Name = "LE VAN QUY",
+                    StreetAddress="280 An Dương Vương",
+                    Ward="Phường 4",
+                    District ="Quận 5",
+                    Province ="Tp. Hồ Chí Minh",
+                    PostalCode="12121",
+                    PhoneNumber = "7779990000"
+                }
+                );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Title = "Bút Bi Thiên Long",
+                    Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+                    ListPrice = 99,
+                    Price = 90,
+                    Price50 = 85,
+                    Price100 = 80,
+                    CategoryId = 1
+                  
+                },
+                new Product
+                {
+                    Id = 2,
+                    Title = "Bút lông dầu Thiên Long",
+                    Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+                    ListPrice = 40,
+                    Price = 30,
+                    Price50 = 25,
+                    Price100 = 20,
+                    CategoryId = 1
+                   
+                },
+                new Product
+                {
+                    Id = 3,
+                    Title = "TẬP HỌC SINH 96 TRANG 4 Ô LY VUÔNG 70 GSM ĐIỂM 10",
+                    Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+                    ListPrice = 55,
+                    Price = 50,
+                    Price50 = 40,
+                    Price100 = 35,
+                    CategoryId = 2
+                    
+                },
+                new Product
+                {
+                    Id = 4,
+                    Title = "TẬP HỌC SINH 96 TRANG 5 Ô LY VUÔNG 80 GSM ĐIỂM 10",
+                    Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+                    ListPrice = 70,
+                    Price = 65,
+                    Price50 = 60,
+                    Price100 = 55,
+                    CategoryId = 2
+                    
+                },
+                new Product
+                {
+                    Id = 5,
+                    Title = "Thước dẻo PVC 20cm Điểm 10",
+                    Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+                    ListPrice = 30,
+                    Price = 27,
+                    Price50 = 25,
+                    Price100 = 20,
+                    CategoryId = 3
+                    
+                },
+                new Product
+                {
+                    Id = 6,
+                    Title = "Thước dẻo PVC 30cm Điểm 10",
+                    Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+                    ListPrice = 25,
+                    Price = 23,
+                    Price50 = 22,
+                    Price100 = 20,
+                    CategoryId = 3
+                    
+                }
+                );
+        }
+    }
+}
---
+++ Telescope/Program.cs
@@ -0,0 +1,84 @@
+using Microsoft.EntityFrameworkCore;
+using Telescope.DataAccess.Data;
+using Telescope.DataAccess.Repository.IRepository;
+using Telescope.DataAccess.Repository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Telescope.Utility;
+using Stripe;
+using Telescope.DataAccess.DbInitializer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Stripe"));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+builder.Services.ConfigureApplicationCookie(options => {
+    options.LoginPath = $"/Identity/Account/Login";
+    options.LogoutPath = $"/Identity/Account/Logout";
+    options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+});
+
+builder.Services.AddAuthentication().AddFacebook(option =>
+{
+    option.AppId = "370447575430082";
+    option.AppSecret = "4e21eb003898659046d0f9cd55296ff5";
+});
+
+builder.Services.AddAuthentication().AddMicrosoftAccount(option =>
+{
+    option.ClientId = "da3937b0-de7c-42d3-8920-9268033b70d7";
+    option.ClientSecret = "JO98Q~s1LmZ1P~oL7XE2KxeEPlHMhOTupuArvbrm";
+});
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options => {
+    options.IdleTimeout = TimeSpan.FromMinutes(100);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+builder.Services.AddRazorPages();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IEmailSender, EmailSender>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseSession();
+SeedDatabase();
+app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+void SeedDatabase()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+        dbInitializer.Initialize();
+    }
+}
